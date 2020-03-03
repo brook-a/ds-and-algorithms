63e6207a5bfebe18a7ec5e6257cfd0984c02687a
brook ambaw <brook.ambaw1@gmail.com>
2020-03-03 15:08:17 -0600

    chore: added funtion implementation

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -1,4 +1,27 @@
 
+/* (easy)
+  say you have an array for which the ith element is the price of a given stock on day i.
+  if you were only permitted to complete at most one transaction (i.e., buy one and sell
+  one share of the stock), design an algorithm to find the maximum profit
+*/
 public class BestTimeToBuyAndSellStock {
 
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.printf("max profit is %d", maxProfit(prices));
+
+    }
+
+    static int maxProfit(int[] prices) {
+
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; ++i) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
 }
