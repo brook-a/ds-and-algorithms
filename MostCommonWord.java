f20356c7d39064bd1d412f9a64d821bc4683f080
brook ambaw <brook.ambaw1@gmail.com>
2020-03-03 12:03:31 -0600

    most common word (easy)

--- MostCommonWord.java
+++ MostCommonWord.java
@@ -7,5 +7,46 @@
  */
 
 
+import java.util.*;
+
 public class MostCommonWord {
+    public static void main(String[] args) {
+        String[] banned = {"hit"};
+        String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+        System.out.printf("the most common word is : %s", mostCommonWord(paragraph, banned));
+    }
+
+    static String mostCommonWord(String para, String[] banned) {
+
+        String[] words = para.toLowerCase().split("\\W+");
+        Set<String> banned_set = new HashSet<>();
+        Map<String, Integer> frequency = new HashMap<>();
+
+        for (String s : banned) {
+            banned_set.add(s);
+        }
+
+        for (String w : words) {
+            if (frequency.containsKey(w)) {
+                frequency.put(w, frequency.get(w) + 1);
+            } else {
+                frequency.put(w, 1);
+            }
+        }
+
+        String common = "";
+        int max = 0;
+
+        for (String s : frequency.keySet()) {
+            if (banned_set.contains(s)) {
+                continue;
+            }
+            if (frequency.get(s) > max) {
+                max = frequency.get(s);
+                common = s;
+            }
+        }
+
+        return common;
+    }
 }
